05ef5c73e470df118dcb01f3a6aecbbf00854a92
Karthik G. Appiah <122487930+karthikgappiah@users.noreply.github.com>
2026-06-09 01:19:42 -0700

    feat: add scalar ui

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { OpenAPIHono as Hono } from "@hono/zod-openapi";
+import { Scalar } from "@scalar/hono-api-reference";
 
 import manifest from "#/package.json" with { type: "json" };
 import { index_route } from "#src/routes/index.js";
@@ -17,5 +18,7 @@ app.doc("/spec", {
   },
   openapi: "3.0.0",
 });
+
+app.get("/ui", Scalar({ url: "/spec" }));
 
 export default app;
